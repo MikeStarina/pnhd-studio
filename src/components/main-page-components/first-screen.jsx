55fa27b0b167e387cb15c613b1d254f2072c914a
Mike Starina <x@pnhd.ru>
2022-09-29 18:00:08 +0300

    first screen bg video added

--- src/components/main-page-components/first-screen.jsx
+++ src/components/main-page-components/first-screen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './first-screen.module.css';
 import { Link } from "react-router-dom";
+import bgvideo from '../../bgvideo.mp4';
 
 
 
@@ -9,6 +10,10 @@ import { Link } from "react-router-dom";
 const FirstScreen = () => {
     return (
         <section className={styles.main_screen}>
+            <video autoplay='autoplay' muted='muted' loop='loop' class={styles.background_video}>
+                <source src={bgvideo} type="video/mp4"></source>
+            </video>
+            <div className={styles.video_overlay}></div>
         <h1 className={styles.main_heading}>НАПЕЧАТАЕМ!</h1>
         <span className={styles.textStyle_italic}>
             <h2 className={styles.main_heading}>НАПЕЧАТАЕМ!</h2>
@@ -21,7 +26,7 @@ const FirstScreen = () => {
                 &nbsp;/трендовых/&nbsp;
             </span>
                  футболках и худи за 15 минут! Клик, чтобы перейти в конструктор!</p>
-        <Link>
+        <Link className={styles.link}>
             <button type='button' className={styles.button}>КОНСТРУКТОР</button>
         </Link>
     </section>
