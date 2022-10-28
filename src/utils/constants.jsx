f6c4dc545bd690c8e0cd66bb4af95c6dac892bdc
Mike Starina <x@pnhd.ru>
2022-10-28 18:43:37 +0300

    some api fixes

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,3 @@
-export const apiBaseUrl = 'http://194.58.122.162:1337';
+export const apiBaseUrl = 'https://pnhdstudioapi.ru';
+//export const apiBaseUrl = 'http://194.58.122.162:3000';
 //export const apiBaseUrl = 'http://localhost:1337';
