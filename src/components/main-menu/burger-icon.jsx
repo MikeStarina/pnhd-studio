ea8d51221c5e68d069dd6a92a492f3a2d97b074f
Mike Starina <x@pnhd.ru>
2023-02-08 17:38:59 +0300

    phone number fixed

--- src/components/main-menu/burger-icon.jsx
+++ src/components/main-menu/burger-icon.jsx
@@ -9,7 +9,7 @@ const BurgerIcon = ({ openMenu, openPopup }) => {
 
     return (
         <div className={styles.burger}>
-            <a href='tel:+78129046156' className={styles.link}>/ 8 (812) 904-62-56</a>
+            <a href='tel:+78129046156' className={styles.link}>/ 8 (812) 904-61-56</a>
             <a href='https://t.me/pnhd_studio' className={styles.link} target='blank'>/ TG</a>
             <a href='whatsapp://send?phone=79313566552' className={styles.link} target='blank'>/ WA</a>
             <button className={styles.button} onClick={openPopup}>ПЕРЕЗВОНИТЬ?</button>
