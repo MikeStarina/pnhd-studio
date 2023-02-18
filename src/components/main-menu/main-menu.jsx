e1de6053458589d116d7dbacdc0e4181c1222a4d
Mike Starina <x@pnhd.ru>
2023-02-19 00:55:32 +0300

    added some features

--- src/components/main-menu/main-menu.jsx
+++ src/components/main-menu/main-menu.jsx
@@ -9,18 +9,21 @@ import { useSelector } from "react-redux";
 
 const MainMenu = ({ closeMenu }) => {
 
-    //const { userAuth } = useSelector(store => store.userData);
-
+    const clickOnEmptyHandler = (e) => {
+        if (e.target.id === 'background') {
+            closeMenu();
+        }
+    }
+ 
     return (
-        <div className={styles.menu_screen}>
+        <div className={styles.menu_screen} onClick={clickOnEmptyHandler} id='background'>
             <div className={styles.menu_box} >
                 <div className={styles.wrapper}>
                     <button type='button' className={styles.close_button} onClick={closeMenu}>
                         <img src={closeIcon} alt='close icon' className={styles.close_icon}></img>
                     </button>
-                    <Link className={styles.menu_link} to='/'>ГЛАВНАЯ</Link>
-                    <Link className={styles.menu_link} to='/shop'>КАТАЛОГ</Link>
-                    <Link className={styles.menu_link} to='/oferta'>ОФЕРТА</Link>
+                    <Link className={styles.menu_link} to='/' onClick={closeMenu}>ГЛАВНАЯ</Link>
+                    <Link className={styles.menu_link} to='/shop' onClick={closeMenu}>КОНСТРУКТОР</Link>
                     <a className={styles.menu_link} href='https://pnhd.ru' target='blank'>ОПТОВЫЙ ОТДЕЛ</a>
                     <a className={styles.menu_link} href='https://instagram.com/pnhd.studio' target='blank'>ИНСТАГРАМ</a>
                     <a className={styles.menu_link} href='https://vk.com/pinheadspb' target='blank'>ВК</a>
