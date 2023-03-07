9a7e2bfce3276b0627c46e4c38922cca451d9639
Mike Starina <x@pnhd.ru>
2023-03-07 22:21:03 +0300

    helmet updated

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { CLOSE_MODAL_MENU } from "../../services/actions/utility-actions.jsx";
 import styles from "./main-page.module.css";
-
+import { Helmet } from "react-helmet";
 import FirstScreen from "../../components/main-page-components/first-screen.jsx";
 import FaqScreen from "../../components/main-page-components/faq-screen.jsx";
 import ThirdScreen from "../../components/main-page-components/third-screen.jsx";
@@ -30,6 +30,45 @@ const MainPage = () => {
 
   return (
     <main className={styles.main_page}>
+       <Helmet
+                title='PINHEAD STUDIO | Печать на футболках и толстовках | Контакты и цены'
+                script = {[
+                    { 
+                    type: "application/ld+json",
+                    innerHTML:
+                        `{
+                            "@context": "https://schema.org",
+                            "@type": "Organization",
+                            "url": "https://studio.pnhd.ru",
+                            "logo": "/icon_logo.svg",
+                            "address": {
+                                "@type": "PostalAddress",
+                                "streetAddress": "ул. Чапыгина 1",
+                                "addressLocality": "Санкт-Петербург",
+                                "addressRegion": "RU",
+                                "postalCode": "197022",
+                                "addressCountry": "RU"
+                            },
+                            "contactPoint" : [
+                            {
+                            "@type" : "ContactPoint",
+                            "telephone" : "+78129046156",
+                            "contactType" : "студия"
+                            }
+                            ],
+                            "sameAs": [
+                              'https://vk.com/pinheadspb',
+                              'https://instagram.com/pnhd.studio'
+                            ]
+                        }
+                        }
+                        }`
+                    }
+                    
+                ]}
+
+
+        />
       <FirstScreen />
       <FaqScreen />
       <ThirdScreen />
