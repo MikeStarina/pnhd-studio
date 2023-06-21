3a633e859f09a07ecf440422f873e0dfb59a6049
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2023-06-21 10:44:36 +0400

    task: added links in gallery and footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,23 +1,66 @@
-import React, { useEffect } from "react";
+import React from 'react';
 import styles from './footer.module.css';
-import { Link } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
-
-
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 const Footer = () => {
-
-    
-    
     return (
-        <footer className={styles.footer} id='footer'>
-            <a className={styles.footer_link} href='https://pnhd.ru' target='blank'>Оптовый отдел</a>
-            
-            <Link className={styles.footer_link} to='/shop'>Каталог</Link>
-            <HashLink className={styles.footer_link} to='/#contacts'>Контакты</HashLink>
-            <Link className={styles.footer_link} to='/oferta'>Оферта</Link>
+        <footer className={styles.footer} id="footer">
+            <Link
+                className={styles.footer_link}
+                to="https://pnhd.ru"
+                target="blank"
+            >
+                Оптовый отдел
+            </Link>
+            <Link className={styles.footer_link} to="/shop">
+                Каталог
+            </Link>
+            <HashLink className={styles.footer_link} to="/#contacts">
+                Контакты
+            </HashLink>
+            <Link className={styles.footer_link} to="/oferta">
+                Оферта
+            </Link>
+            <ul className={styles.list}>
+                <li>
+                    <Link
+                        className={styles.item}
+                        to="/termotransfernaya-pechat"
+                    >
+                        Флекс
+                    </Link>
+                </li>
+                <li>
+                    <Link className={styles.item} to="/vishivka">
+                        Вышивка
+                    </Link>
+                </li>
+                <li>
+                    <Link className={styles.item} to="/shelkografiya">
+                        Шелкография
+                    </Link>
+                </li>
+                <li>
+                    <Link className={styles.item} to="/pryamaya-dtg-pechat">
+                        DTG
+                    </Link>
+                </li>
+                <li>
+                    <Link className={styles.item} to="/dtf-pechat">
+                        DTF
+                    </Link>
+                </li>
+            </ul>
+            <ul className={styles.list}>
+                <li className={styles.item}>Печать изображений</li>
+                <li className={styles.item}>Печать фото</li>
+                <li className={styles.item}>Печать надписей</li>
+                <li className={styles.item}>Печать имени и номера</li>
+                <li className={styles.item}>Печать логотипов</li>
+            </ul>
         </footer>
     );
-}
+};
 
 export default Footer;
