c94516c936de1db3a8054dcbe062fc0265a3a77a
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2023-06-27 14:51:39 +0400

    bagfix: change onCkick on onMouseDown and added esc close popup

--- src/components/popupModel/popupModel.jsx
+++ src/components/popupModel/popupModel.jsx
@@ -1,30 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import styles from './popupModel.module.css';
-import { useSelector, useDispatch } from 'react-redux';
 
 const PopupModel = ({ openPopup, children }) => {
-    const dispatch = useDispatch();
-    const { orderFormData, error } = useSelector((store) => store.utilityState);
-    const [isMessageSent, setIsMessageSent] = useState(false);
-    const [isError, setIsError] = useState('');
-
     const closeByBGClick = (e) => {
         if (e.target.id === 'container') {
             openPopup();
         }
     };
 
+    const handleEscKeydown = (e) => {
+        e.key === 'Escape' && openPopup();
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleEscKeydown);
+        return () => {
+            document.removeEventListener('keydown', handleEscKeydown);
+        };
+    }, []);
+
     return (
         <div
             className={styles.popup_container}
-            onClick={closeByBGClick}
+            onMouseDown={closeByBGClick}
             id="container"
         >
             <div className={styles.lead}>
                 <button
                     type="button"
                     className={styles.close_button}
-                    onClick={openPopup}
+                    onMouseDown={openPopup}
                 >
                     X
                 </button>
