d02f463bd66efe36d52c65b9af56312f241998a5
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2023-08-02 15:38:41 +0400

    deleted WA from burger-icon

--- src/components/main-menu/burger-icon.jsx
+++ src/components/main-menu/burger-icon.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React from 'react';
 import styles from './burger-icon.module.css';
 import burger from '../images/burger.svg';
-import { contactPhone } from "../../utils/constants";
-
+import { contactPhone } from '../../utils/constants';
 
 const BurgerIcon = ({ openMenu, openPopup }) => {
-
-    
-
-    return (
-        <div className={styles.burger}>
-            <a href='tel:+78129046156' className={styles.link} id='calltracking'>{contactPhone}</a>
-            <a href='https://t.me/pnhd_studio' className={styles.link} target='blank'>/ TG</a>
-            <a href='whatsapp://send?phone=79313566552' className={styles.link} target='blank'>/ WA</a>
-            <button className={styles.button} onClick={openPopup}>ПЕРЕЗВОНИТЬ?</button>
-            <img src={burger} alt='main menu' className={styles.burger_icon} onClick={openMenu}></img>
-        </div>
-    );
-
-
-}
-
+  return (
+    <div className={styles.burger}>
+      <a href="tel:+78129046156" className={styles.link} id="calltracking">
+        {contactPhone}
+      </a>
+      <a href="https://t.me/pnhd_studio" className={styles.link} target="blank">
+        / TG
+      </a>
+      <button className={styles.button} onClick={openPopup}>
+        ПЕРЕЗВОНИТЬ?
+      </button>
+      <img
+        src={burger}
+        alt="main menu"
+        className={styles.burger_icon}
+        onClick={openMenu}
+      ></img>
+    </div>
+  );
+};
 
 export default BurgerIcon;
