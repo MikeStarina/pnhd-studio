a0197162df5248a4ec55f25d9d5fca22d24302ac
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2023-08-11 15:27:17 +0400

    refactor: added new footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,83 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import styles from './footer.module.css';
+import { PayCardsLabel } from '../../ui/icons/pay-cards-label';
+import { Vk } from '../../ui/icons/vk';
+import { Telegram } from '../../ui/icons/telegram';
+import { footerData } from '../../data/footer/data';
 
 function Footer() {
   return (
     <footer className={styles.footer} id="footer">
-      <Link
-        className={styles.footer_link}
-        to="https://pnhd.ru"
-        target="blank"
-      >
-        Оптовый отдел
-      </Link>
-      <Link className={styles.footer_link} to="/shop">
-        Каталог
-      </Link>
-      <HashLink className={styles.footer_link} to="/#contacts">
-        Контакты
-      </HashLink>
-      <Link className={styles.footer_link} to="/oferta">
-        Оферта
-      </Link>
-      <ul className={styles.list}>
-        <li>
-          <Link
-            className={styles.item}
-            to="/termotransfernaya-pechat"
-          >
-            Флекс
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.item} to="/vishivka">
-            Вышивка
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.item} to="/shelkografiya">
-            Шелкография
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.item} to="/pryamaya-dtg-pechat">
-            DTG
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.item} to="/dtf-pechat">
-            DTF
-          </Link>
-        </li>
-      </ul>
-      <ul className={styles.list}>
-        <li>
-          <Link className={styles.item} to="/pechat-printov">
-            Печать изображений
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.item} to="/pechat-photo">
-            Печать фото
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.item} to="/pechat-nadpisej">
-            Печать надписей
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.item} to="/pechat-familii">
-            Печать имени и номера
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.item} to="/pechat-logotipa">
-            Печать логотипов
-          </Link>
-        </li>
-      </ul>
+      {footerData.map((item, index) => (
+        <div className={styles.item_block} style={{ gridArea: item.styles }} key={index}>
+          <p className={styles.block_menu}>{item.name}</p>
+          <ul className={styles.block_info}>
+            {item.list.map((element, ind) => (
+              <li key={ind}>
+                <HashLink to={element.link} target={element.target} className={styles.info_link}>
+                  {element.type}
+                </HashLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <div className={styles.block_label}>
+        <PayCardsLabel />
+        <p className={styles.label_info}>ООО “ПИНХЭД СТУДИО”</p>
+        <p className={styles.label_info}>ИНН: 7810463916</p>
+        <p className={styles.label_info}>ОГРН: 1137847215440</p>
+        <div>
+          <Link to={{ pathname: 'https://vk.com/pinheadspb' }} className={styles.social_link} target="blank"><Vk className={styles.social} /></Link>
+          <Link to={{ pathname: 'https://t.me/pnhd_studio' }} className={styles.social_link} target="blank"><Telegram className={styles.social} /></Link>
+        </div>
+      </div>
+      <div className={styles.block_sign}>
+        <p className={styles.sign_text}>MERCH AGAINST THE MACHINES!</p>
+        <p className={styles.sign_text}>PINHEAD STUDIO 2023</p>
+      </div>
     </footer>
   );
 }
