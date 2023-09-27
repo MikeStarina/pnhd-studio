aec0d9f06105693392309efde1cc9ec24294d116
VadimLitau <vadimlitau@yandex.ru>
2023-09-27 13:35:45 +0600

    ост-ся на PrintingFaq

--- src/components/printing-faq/printing-faq.jsx
+++ src/components/printing-faq/printing-faq.jsx
@@ -4,12 +4,12 @@ import circle50px from '../images/circle50px.png';
 
 function PrintingFaq(data) {
   const { title, description, variants } = data;
-
+  console.log(description);
   return (
     <section className={styles.screen}>
       <h3 className={styles.heading}>{title}</h3>
-      {variants.map((item, index) => (
-        <div className={styles.block_wrapper} key={index}>
+      {variants.map((item) => (
+        <div className={styles.block_wrapper} key={item.screen_heading}>
           <img
               src={circle50px}
               className={styles.screen_circle}
@@ -21,6 +21,7 @@ function PrintingFaq(data) {
               item.screen_description.map((item, index) => (
                 <p
                       className={`${styles.screen_description} ${styles.screen_description_bottom}`}
+                      // eslint-disable-next-line react/no-array-index-key
                       key={index}
                 >
                   {item}
@@ -37,6 +38,7 @@ function PrintingFaq(data) {
       {Array.isArray(description) && (
         <>
           {description.map((item, index) => (
+            // eslint-disable-next-line react/no-array-index-key
             <div className={styles.block_wrapper} key={index}>
               <p className={styles.bottom_description}>{item}</p>
             </div>
--- src/data/footer/data.js
+++ src/data/footer/data.js
@@ -1,26 +1,32 @@
 export const footerData = [
   {
+    id: '0',
     name: 'Клиентам',
     styles: 'customer',
     list: [
       {
+        id: '0',
         type: 'Оптовый отдел',
         link: { pathname: 'https://pnhd.ru' },
         target: '_blank',
       },
       {
+        id: '1',
         type: 'Каталог',
         link: '/shop',
       },
       {
+        id: '2',
         type: 'Контакты',
         link: '#contacts',
       },
       {
+        id: '3',
         type: 'Оферта',
         link: '/oferta',
       },
       {
+        id: '4',
         type: 'Гид по размерам',
         link: '/size_chart',
         target: '_blank',
@@ -28,81 +34,99 @@ export const footerData = [
     ],
   },
   {
+    id: '1',
     name: 'Печать',
     styles: 'print',
     style: 'styles.block_print',
     list: [
       {
+        id: '0',
         type: 'Флекс',
         link: '/termotransfernaya-pechat',
       },
       {
+        id: '1',
         type: 'Вышивка',
         link: '/vishivka',
       },
       {
+        id: '2',
         type: 'Шелкография',
         link: '/shelkografiya',
       },
       {
+        id: '3',
         type: 'DTG',
         link: '/pryamaya-dtg-pechat',
       },
       {
+        id: '4',
         type: 'DTF',
         link: '/dtf-pechat',
       },
     ],
   },
   {
+    id: '2',
     name: 'Принты',
     styles: 'printing',
     style: 'styles.block_printing',
     list: [
       {
+        id: '0',
         type: 'Печать изображений',
         link: '/pechat-printov',
       },
       {
+        id: '1',
         type: 'Печать фото',
         link: '/pechat-photo',
       },
       {
+        id: '2',
         type: 'Печать надписей',
         link: '/pechat-nadpisej',
       },
       {
+        id: '3',
         type: 'Печать имени и номера',
         link: '/pechat-familii',
       },
       {
+        id: '4',
         type: 'Печать логотипов',
         link: '/pechat-logotipa',
       },
     ],
   },
   {
+    id: '3',
     name: 'Текстиль',
     styles: 'textile',
     style: 'styles.block_textile',
     list: [
       {
+        id: '0',
         type: 'Печать на футболках',
         link: '/',
       },
       {
+        id: '1',
         type: 'Печать на худи',
         link: '/',
       },
       {
+        id: '2',
         type: 'Печать на свитшотах',
         link: '/',
       },
       {
+        id: '3',
         type: 'Печать на шопперах',
         link: '/',
       },
       {
+        id: '4',
         type: 'Печать кепках',
         link: '/',
       },
