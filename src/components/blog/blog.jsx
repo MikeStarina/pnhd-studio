0dddc59e13b956b7fcb781664bfb68d3827f5044
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2023-10-31 10:33:39 +0100

    feat: added imege in cart blog

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './blog.module.css';
+import { apiBaseUrl } from '../../utils/constants';
 
 function Blog({ ...el }) {
+  const photo = el.blog.find((item) => item.image);
   return (
     <div className={styles.block}>
-      <h3 className={styles.title}>{el.title}</h3>
+      <h3 className={styles.title} style={{ backgroundImage: `url(${apiBaseUrl}${photo && photo.image})` }}>{el.title}</h3>
     </div>
   );
 }
