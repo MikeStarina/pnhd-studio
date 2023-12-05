1c8b09f697079c8c35fb17080d016cc39e44d8f4
Mike Starina <x@pnhd.ru>
2023-12-05 15:09:05 +0300

    add lint fix

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import styles from './footer.module.css';
 import PayCardsLabel from '../../ui/icons/pay-cards-label';
-import Vk from '../../ui/icons/vk';
-import Telegram from '../../ui/icons/telegram';
+// import Vk from '../../ui/icons/vk';
+// import Telegram from '../../ui/icons/telegram';
 import footerData from '../../data/footer/data';
-import { telegram, vkontakte } from '../../utils/constants';
+// import { telegram, vkontakte } from '../../utils/constants';
 
 function Footer() {
   return (
     <footer className={styles.footer} id="footer">
       {footerData.map((item) => (
-        <div className={styles.item_block} style={{ gridArea: item.styles }} key={item.id}>
+        <div
+          className={styles.item_block}
+          style={{ gridArea: item.styles }}
+          key={item.id}
+        >
           <p className={styles.block_menu}>{item.name}</p>
           <ul className={styles.block_info}>
             {item.list.map((element) => (
               <li key={element.id}>
-                <HashLink to={element.link} target={element.target} className={styles.info_link}>
+                <HashLink
+                  to={element.link}
+                  target={element.target}
+                  className={styles.info_link}
+                >
                   {element.type}
                 </HashLink>
               </li>
@@ -32,8 +40,16 @@ function Footer() {
         <p className={styles.label_info}>ОГРН: 1137847215440</p>
         {/*
         <div>
-          <Link to={{ pathname: vkontakte }} className={styles.social_link} target="blank"><Vk className={styles.social} /></Link>
-          <Link to={{ pathname: telegram }} className={styles.social_link} target="blank"><Telegram className={styles.social} /></Link>
+          <Link
+            to={{ pathname: vkontakte }}
+            className={styles.social_link}
+            target="blank"><Vk
+            className={styles.social}
+          />
+          <Link
+          to={{ pathname: telegram }}
+          className={styles.social_link}
+          target="blank"><Telegram className={styles.social} /></Link>
         </div>
             */}
       </div>
