2f8e00f9dd7d6e3372657deecf5fcdc7407afd5d
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2023-12-05 20:27:34 +0100

    fex: console

--- src/data/photo-processing/photo-processing.js
+++ src/data/photo-processing/photo-processing.js
@@ -1,7 +1,7 @@
 const photoProcessing = (file) => {
   // Размер ф-ла, нужен для отображения загрузчика
   // const size = file.size ? file.size : 'NOT SUPPORTED';
-  console.log(file);
+  // console.log(file);
   const type = file.type ? file.type : 'NOT SUPPORTED';
   if (type === 'image/png' || type === 'image/jpeg') {
     return file;
