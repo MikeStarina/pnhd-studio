742c7cd3cc42db78e1306d799f21780cf4934eed
Mike Starina <x@pnhd.ru>
2024-04-08 15:12:21 +0300

    roistat script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import CartIcon from "@/components/shared-components/cart-icon/cart-icon";
 import Popup from "@/components/shared-components/popup/popup";
 import Script from "next/script";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -36,10 +35,17 @@ export default function RootLayout({
             {children}
             <Footer />
           </main>
-        </body>
-      </ReduxProvider>
-      <Script async id="metrika-counter" strategy="afterInteractive">
-        {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          <Script>
+            {`(function(w, d, s, h, id) {
+                w.roistatProjectId = id; w.roistatHost = h;
+                var p = d.location.protocol == "https:" ? "https://" : "http://";
+                var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+                var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+            })(window, document, 'script', 'cloud.roistat.com', 'd377e83a9c9ee68536343fd69de092e3');`
+          }
+          </Script>
+          <Script async id="metrika-counter" strategy="afterInteractive">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -54,9 +60,20 @@ export default function RootLayout({
           });
           window.dataLayer = window.dataLayer || [];
           `}
-      </Script>
-      <Script src="//cdn.callibri.ru/callibri.js" async id="calltr"></Script>
-      <Script type="application/javascript" async src="//tagmanager.andata.ru/api/v1/container/9b6370c8-6f89-4792-b816-520e886444ad/published/code.js"></Script>
+          </Script>
+          <Script
+            src="//cdn.callibri.ru/callibri.js"
+            async
+            id="calltr"
+          ></Script>
+          <Script
+            type="application/javascript"
+            async
+            src="//tagmanager.andata.ru/api/v1/container/9b6370c8-6f89-4792-b816-520e886444ad/published/code.js"
+          ></Script>
+        </body>
+      </ReduxProvider>
+
       {/* <Script>
         {`(function(w,d,u){
                 var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
